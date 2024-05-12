2335871cbd9e51e93622487ec450a8b222be041d
fewgydhu <ssjcemajbrickus@hotmail.com>
2024-05-12 21:36:59 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const lcm = (a, b) => {
-  return (a * b) / gcd(a, b);
-};
+function productExceptSelf(nums) {
+  const result = [];
+  let product = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = product;
+    product *= nums[i];
+  }
+  product = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+  return result;
+}
