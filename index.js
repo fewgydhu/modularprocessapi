2f320057bfaf3ba0f5f1b7ddc34837ccfdf5640e
fewgydhu <ssjcemajbrickus@hotmail.com>
2024-05-16 16:07:37 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-function sortedArrayToBST(nums) {
-  if (!nums.length) return null;
-  const mid = Math.floor(nums.length / 2);
-  const root = new TreeNode(nums[mid]);
-  root.left = sortedArrayToBST(nums.slice(0, mid));
-  root.right = sortedArrayToBST(nums.slice(mid + 1));
-  return root;
+function combinationSum3(k, n) {
+  const result = [];
+  backtrack([], 1, k, n);
+  return result;
+  function backtrack(combination, start, k, n) {
+    if (n === 0 && k === 0) {
+      result.push([...combination]);
+      return;
+    }
+    if (n < 0 || k === 0) return;
+    for (let i = start; i <= 9; i++) {
+      combination.push(i);
+      backtrack(combination, i + 1, k - 1, n - i);
+      combination.pop();
+    }
+  }
 }
